6f03fab35b733271e27a3572b34088665bd3b290
vladkha <vladislavkhaitov@gmail.com>
2017-02-12 00:15:59 +0200

    Cleaned-up pour problem

--- Week4/pour_problem.py
+++ Week4/pour_problem.py
@@ -1,5 +1,6 @@
 import doctest
 
+
 def pour_problem(X, Y, goal, start=(0, 0)):
     """X and Y are the capacity of glasses; (x,y) is current fill levels
     and represents a state. The goal is a level that can be in either glass.
@@ -7,11 +8,11 @@ def pour_problem(X, Y, goal, start=(0, 0)):
     Keep track of frontier and previously explored; fail when no frontier."""
     if goal in start:
         return [start]
-    explored = set()        # set of states we have visited
-    frontier = [[start]]    # ordered list of paths we have blazed
+    explored = set()  # set of states we have visited
+    frontier = [[start]]  # ordered list of paths we have blazed
     while frontier:
         path = frontier.pop(0)
-        (x, y) = path[-1]   # last state in the first path of the frontier
+        (x, y) = path[-1]  # last state in the first path of the frontier
         for (state, action) in successors(x, y, X, Y).items():
             if state not in explored:
                 explored.add(state)
@@ -22,6 +23,7 @@ def pour_problem(X, Y, goal, start=(0, 0)):
                     frontier.append(path2)
     return Fail
 
+
 Fail = []
 
 
@@ -29,8 +31,8 @@ def successors(x, y, X, Y):
     """Return a dictionary of {state:action} pairs describing what can be
     reached from the (x,y) state, and how"""
     assert x <= X and y <= Y  # (x, y) is glass levels; X and Y are glass sizes
-    return {((0, y+x) if y+x <= Y else (x-(Y-y), y+(Y-y))): 'X->Y',
-            ((x+y, 0) if x+y <= X else (x+(X-x), y-(X-x))): 'X<-Y',
+    return {((0, y + x) if y + x <= Y else (x - (Y - y), y + (Y - y))): 'X->Y',
+            ((x + y, 0) if x + y <= X else (x + (X - x), y - (X - x))): 'X<-Y',
             (X, y): 'fill X',
             (x, Y): 'fill Y',
             (0, y): 'empty X',
@@ -44,6 +46,7 @@ class Test: """
 >>> pour_problem(4, 9, 6)
 [(0, 0), 'fill Y', (0, 9), 'X<-Y', (4, 5), 'empty X', (0, 5), 'X<-Y', (4, 1), 'empty X', (0, 1), 'X<-Y', (1, 0), 'fill Y', (1, 9), 'X<-Y', (4, 6)]
     """
+
 
 # print(pour_problem(4, 9, 5))
 # print(pour_problem(4, 9, 6))
