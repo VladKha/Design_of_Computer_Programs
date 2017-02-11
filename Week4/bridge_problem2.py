07bfa22bd10bfd2ac52ecdff4a4159403beb1abc
vladkha <vladislavkhaitov@gmail.com>
2017-02-11 17:48:29 +0200

    Finished second solution to Bridge problem

--- Week4/bridge_problem2.py
+++ Week4/bridge_problem2.py
@@ -1,15 +1,3 @@
-# -----------------
-# User Instructions
-#
-# write a function, bsuccessors2 that takes a state as input
-# and returns a dictionary of {state:action} pairs.
-#
-# The new representation for a path should be a list of
-# [state, (action, total time), state, ... , ], though this
-# function will just return {state:action} pairs and will
-# ignore total time.
-#
-# The previous bsuccessors function is included for your reference.
 
 def bsuccessors2(state):
     """Return a dict of {state:action} pairs. A state is a
@@ -38,6 +26,26 @@ def bsuccessors2(state):
     return result
 
 
+def path_cost(path):
+    """The total cost of a path (which is stored in a tuple
+    with the final action."""
+    # path = (state, (action, total_cost), state, ... )
+    if len(path) < 3:
+        return 0
+    else:
+        action, total_cost = path[-2]
+        return total_cost
+
+
+def bcost(action):
+    """Returns the cost (a number) of an action in the
+    bridge problem."""
+    # An action is an (a, b, arrow) tuple; a and b are
+    # times; arrow is a string.
+    a, b, arrow = action
+    return max(a, b)
+
+
 def test():
     here1 = frozenset([1, 'light'])
     there1 = frozenset([])
@@ -51,6 +59,12 @@ def test():
             (frozenset([1]), frozenset(['light', 2, 3])): (2, 2, '->'),
             (frozenset([2]), frozenset([1, 3, 'light'])): (1, 1, '->'),
             (frozenset([]), frozenset([1, 2, 3, 'light'])): (2, 1, '->')}
+
+    assert path_cost(('fake_state1', ((2, 5, '->'), 5), 'fake_state2')) == 5
+    assert path_cost(('fs1', ((2, 1, '->'), 2), 'fs2', ((3, 4, '<-'), 6), 'fs3')) == 6
+    assert bcost((4, 2, '->'), ) == 4
+    assert bcost((3, 10, '<-'), ) == 10
+
     return 'tests pass'
 
 print(test())
