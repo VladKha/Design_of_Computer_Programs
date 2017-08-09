616435dfc034287fca14443c6d01929d0d6d4b13
vladkha <vladislavkhaitov@gmail.com>
2017-08-09 22:03:13 +0300

    Add 'clueless' function'

--- Week5/pig.py
+++ Week5/pig.py
@@ -42,3 +42,21 @@ def test():
 
 
 print(test())
+
+
+# -----------------
+# User Instructions
+#
+# Write a strategy function, clueless, that ignores the state and
+# chooses at random from the possible moves (it should either
+# return 'roll' or 'hold'). Take a look at the random library for
+# helpful functions.
+
+import random
+
+possible_moves = ['roll', 'hold']
+
+
+def clueless(state):
+    """A strategy that ignores the state and chooses at random from possible moves."""
+    return random.choice(possible_moves)
