eecd45d85426ba687f82d03f6dd7ddaaf93bc83c
vladkha <vladislavkhaitov@gmail.com>
2017-08-10 21:51:07 +0300

    Add 'hold_at' function

--- Week5/pig.py
+++ Week5/pig.py
@@ -60,3 +60,35 @@ possible_moves = ['roll', 'hold']
 def clueless(state):
     """A strategy that ignores the state and chooses at random from possible moves."""
     return random.choice(possible_moves)
+
+
+# -----------------
+# User Instructions
+#
+# In this problem, you will complete the code for the hold_at(x)
+# function. This function returns a strategy function (note that
+# hold_at is NOT the strategy function itself). The returned
+# strategy should hold if and only if pending >= x or if the
+# player has reached the goal.
+
+goal = 50
+
+
+def hold_at(x):
+    """Return a strategy that holds if and only if
+    pending >= x or player reaches goal."""
+    def strategy(state):
+        (p, me, you, pending) = state
+        return 'hold' if (pending >= x or me + pending >= goal) else 'roll'
+    strategy.__name__ = 'hold_at(%d)' % x
+    return strategy
+
+
+def test():
+    assert hold_at(30)((1, 29, 15, 20)) == 'roll'
+    assert hold_at(30)((1, 29, 15, 21)) == 'hold'
+    assert hold_at(15)((0, 2, 30, 10))  == 'roll'
+    assert hold_at(15)((0, 2, 30, 15))  == 'hold'
+    return 'tests pass'
+
+print(test())
