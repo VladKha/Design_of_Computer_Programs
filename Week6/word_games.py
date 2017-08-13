2b2c05ce277484ce4533fd19dbe3c2ddf0c3cadf
vladkha <vladislavkhaitov@gmail.com>
2017-08-13 09:56:33 +0300

    Add topn function

--- Week6/word_games.py
+++ Week6/word_games.py
@@ -1,3 +1,12 @@
+# -----------------
+# User Instructions
+#
+# Write a function, topn, that takes as input a hand, the set of
+# current letters on the board, and a number n, and returns the
+# n best words we can play, sorted by word score.
+# For testing, you can assume that you have access to a file called
+# 'words4k.txt'
+
 import time
 
 
@@ -76,6 +85,20 @@ def longest_words(hand, board_letters):
     """Return all word plays, longest first."""
     words = word_plays(hand, board_letters)
     return sorted(words, key=len, reverse=True)
+
+
+POINTS = dict(A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10, _=0)
+
+
+def word_score(word):
+    """The sum of the individual letter point scores for this word."""
+    return sum(POINTS[L] for L in word)
+
+
+def topn(hand, board_letters, n=10):
+    """Return a list of the top n words that hand can play, sorted by word score."""
+    words = word_plays(hand, board_letters)
+    return sorted(words, key=word_score, reverse=True)[:n]
 
 
 def timedcall(fn, *args):
@@ -161,20 +184,3 @@ def test_words():
 
 
 print(test_words())
-
-
-# -----------------
-# User Instructions
-#
-# Write a function, word_score, that takes as input a word, and
-# returns the sum of the individual letter scores of that word.
-# For testing, you can assume that you have access to a file called
-# 'words4k.txt'
-
-
-POINTS = dict(A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10, _=0)
-
-
-def word_score(word):
-    """The sum of the individual letter point scores for this word."""
-    return sum(POINTS[L] for L in word)
